615791d8932549888750d87efbff3e458bc0a748
Rafath-Bin-Zafar-Auvee <rbza98@outlook.com>
2022-04-18 20:19:42 +0600

    banner

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,39 +1,52 @@
-import { signOut } from "firebase/auth";
-import React from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import auth from "../../firebase.init";
-import CustomLink from "../CustomLink/CustomLink";
-import "./Header.css";
-import logo from "../../images/logo.png"
+// import logo from "../../images/logo.png"
+import { signOut } from 'firebase/auth';
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { Link } from 'react-router-dom';
+import auth from '../../firebase.init';
+import logo from '../../images/logo.png'
+import './Header.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
 const Header = () => {
-  const [user] = useAuthState(auth);
+    const [user] = useAuthState(auth);
 
-  const handleSignOut = () => {
-    signOut(auth);
-  };
-  return (
-    
-    <div>
-    
-      <nav className="header">
-        <img src={logo} alt="" />
-        <div className= "active-link">
-          <CustomLink to="/">Home</CustomLink>
-          <CustomLink to="/about">About</CustomLink>
-          <CustomLink to="/services">Services</CustomLink>
-          <CustomLink to="/blog">Blog</CustomLink>
-          <CustomLink to="/checkout">Checkout</CustomLink>
-          {user ? (
-            <button className="signout" onClick={handleSignOut}>Sign out</button>
-          ) : (
-            <CustomLink to="/login">Login</CustomLink>
-          )}
-          
-        </div>
-      </nav>
-    </div>
-  );
+    const handleSignOut = () =>{
+        signOut(auth);
+    }
+
+    return (
+        <>
+            <Navbar className='sh' collapseOnSelect expand="lg" sticky='top' bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand className='nav-brand sh' as={Link} to="/">
+                        <img height={50} width={100} src={logo} alt="" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            {/* Change korte hobe  */}
+                            <Nav.Link href="/services">Services</Nav.Link>
+                            <Nav.Link as={Link} to="/blog">Blogs</Nav.Link>
+                            <Nav.Link href="/checkout">Checkout</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link as={Link} to="/about">About</Nav.Link>
+                            {
+                                user ?
+                                    <button className='btn btn-link text-white text-decoration-none' onClick={handleSignOut}>Logout  <FontAwesomeIcon icon={faArrowRightFromBracket} /></button>
+                                :
+                                <Nav.Link as={Link} to="/login">
+                                Login
+                            </Nav.Link>}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
 };
 
 export default Header;
