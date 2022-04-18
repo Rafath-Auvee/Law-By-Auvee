0bec1e3e152a18a3081cd36542d4b5f83643ab25
Rafath-Bin-Zafar-Auvee <rbza98@outlook.com>
2022-04-18 23:07:00 +0600

    blog section

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,28 +1,52 @@
-import { Button } from "bootstrap";
-import React from "react";
+import Button from "react-bootstrap/Button";
+import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 
 const Contact = () => {
+  const [validated, setValidated] = useState(false);
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+  };
   return (
     <div>
       <div className="container w-50 mt-5">
         <h1 className="text-success mx-auto align-items-center">Contact Me</h1>
-        <Form>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-            <Form.Label>First Name</Form.Label>
-            <Form.Control type="text" placeholder="name@example.com" />
+        <Form noValidate validated={validated} onSubmit={handleSubmit}>
+          <Form.Group className="mb-3" controlId="">
+            <Form.Label>Your Name</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="First name"
+              defaultValue="Mark"
+            />
           </Form.Group>
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="name@example.com" />
+            <Form.Control type="email" placeholder="Enter email" />
           </Form.Group>
+          
           <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-            <Form.Label>Your Message</Form.Label>
-            <Form.Control as="textarea" rows={3} />
+            <Form.Label>Example textarea</Form.Label>
+            <Form.Control as="textarea" rows={3} required/>
           </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
+          <Form.Group className="mb-3">
+            <Form.Check
+              required
+              label="Agree to terms and conditions"
+              feedback="You must agree before submitting."
+              feedbackType="invalid"
+            />
+          </Form.Group>
+          <Button type="submit">Submit form</Button>
         </Form>
       </div>
     </div>
--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -6,8 +6,8 @@ const Service = ({service}) => {
     const {id, name, img, description, price} = service;
     const navigate = useNavigate();
 
-    const navigateToServiceDetail = id =>{
-        navigate(`/service/${id}`);
+    const navigateToServiceDetail = () =>{
+        navigate(`/checkout`);
     }
     return (
         <div className='service'>
@@ -15,7 +15,7 @@ const Service = ({service}) => {
             <h4>{name}</h4>
             <p>Price: {price}</p>
             <p><small>{description}</small></p>
-            <button onClick={() => navigateToServiceDetail(id)} className='btn btn-success'>Book Now</button>
+            <button onClick={() => navigateToServiceDetail()} className='btn btn-success'>Book Now</button>
         </div>
     );
 };
